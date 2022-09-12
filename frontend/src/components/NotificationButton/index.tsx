ee0acaf5c38637f42c63820fc08e92efdec4042f
Leonardo Nascimento <omgleo@live.com>
2022-09-11 22:25:45 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,26 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './style.css';
 
+type Props = {
+  saleId: number;
+}
 
-function NotificationButton() {
+function handleClick (id:number){
+  axios (`${BASE_URL}/sales/53/notification`)
+  .then(Response =>{
+    console.log("Sucesso");
+  })
+
+
+
+};
+
+
+function NotificationButton({saleId}:Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={()=>handleClick(saleId)}>
         <img src={icon} alt="Notificar" />
       </div>
     )
